45886e4d4d60513f492b126139a786b2571c49e0
Raghavendrakavuru <142613093+Raghavendrakavuru@users.noreply.github.com>
2025-08-29 01:35:14 +0530

    Add files via upload

---
+++ capstone_project/flightdatams/flightdatams/src/main/java/com/wipro/flightdatams/entity/Flight.java
@@ -0,0 +1,34 @@
+package com.wipro.flightdatams.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="flight_data")
+@Data
+public class Flight {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	@Column(name="aircraft_name")
+	private String aircraftName;
+	
+	@Column(name="flight_number")
+	private String flightNumber;
+	
+	
+	private String source;
+	private String destination;
+	private double price;
+	private LocalDate validFrom;
+	private LocalDate validTo;
+}
