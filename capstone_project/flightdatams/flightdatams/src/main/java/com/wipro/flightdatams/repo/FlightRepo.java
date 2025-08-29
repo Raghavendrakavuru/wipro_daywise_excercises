495a41714d8b85d1aa3f024a9f595f3c7a388725
Raghavendrakavuru <142613093+Raghavendrakavuru@users.noreply.github.com>
2025-08-30 01:44:16 +0530

    Delete capstone_project/flightdatams/flightdatams directory

--- capstone_project/flightdatams/flightdatams/src/main/java/com/wipro/flightdatams/repo/FlightRepo.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.flightdatams.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.flightdatams.entity.Flight;
-
-public interface FlightRepo extends JpaRepository<Flight, Long> {
-
-}
