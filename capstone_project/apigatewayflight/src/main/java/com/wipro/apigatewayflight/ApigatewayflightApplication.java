0748eb3c2c8f9fb1eb7ff9f7a0d1ca34e8183e20
Raghavendrakavuru <142613093+Raghavendrakavuru@users.noreply.github.com>
2025-08-30 01:48:48 +0530

    Add files via upload

---
+++ capstone_project/apigatewayflight/src/main/java/com/wipro/apigatewayflight/ApigatewayflightApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.apigatewayflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayflightApplication.class, args);
+	}
+
+}
