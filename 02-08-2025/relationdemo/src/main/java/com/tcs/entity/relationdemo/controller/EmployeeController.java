499c13310437ad44b66a31fb4009a69f5cd9f31e
Raghavendrakavuru <142613093+Raghavendrakavuru@users.noreply.github.com>
2025-08-31 12:04:18 +0530

    Update EmployeeController.java

--- 02-08-2025/relationdemo/src/main/java/com/tcs/entity/relationdemo/controller/EmployeeController.java
+++ 02-08-2025/relationdemo/src/main/java/com/tcs/entity/relationdemo/controller/EmployeeController.java
@@ -1,5 +1,32 @@
 package com.tcs.entity.relationdemo.controller;
 
-public class EmployeeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.entity.relationdemo.entity.Employee;
+import com.tcs.entity.relationdemo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@PostMapping
+	void save(@RequestBody Employee employee){
+		System.out.println(employee);
+		employeeService.save(employee);
+	}
+	
+	@GetMapping
+	List<Employee> findAll() {
+		return employeeService.findAll();
+	}
 }
