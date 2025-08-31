508a802c3b3fd228b8f8d60d67cc17d1234462fb
Raghavendrakavuru <142613093+Raghavendrakavuru@users.noreply.github.com>
2025-08-31 15:17:08 +0530

    Add files via upload

---
+++ 11-08-2025/tokendemo/src/main/java/com/wipro/tokendemo/controller/TokenController.java
@@ -0,0 +1,14 @@
+package com.wipro.tokendemo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController 
+{
+	@RequestMapping("hello")
+	public String getCurrentTime(@RequestParam(value="name", defaultValue="World") String name) {
+		return "Hello "+name+"!!";
+	}
+
+}
